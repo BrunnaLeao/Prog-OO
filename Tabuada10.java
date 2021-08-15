2ce8db6435e952cf89489fd506ac6e5c0e7c0a1b
BrunnaLeao <88848380+BrunnaLeao@users.noreply.github.com>
2021-08-15 16:17:00 -0300

    Add files via upload

---
+++ Tabuada10.java
@@ -0,0 +1,14 @@
+/*Brunna Vila Chã 
+0050016542*/
+class Main {
+    public static void main(String[] args) {
+        int a = 10;
+        for (int i=1;i<11;i++)
+        {
+        int resultado;
+        resultado = a * i;
+            System.out.println(resultado);
+          }
+      }
+    }
+  
